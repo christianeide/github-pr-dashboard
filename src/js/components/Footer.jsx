7bb31a92c1521f02256d69a5a324888889521701
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2019-02-16 18:36:39 +0100

    changed description text

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import pkg from '../../../package.json';
+// import pkg from '../../../package.json';
 
 export default function Footer() {
   return (
     <footer>
       <a target="_blank" href="https://www.github.com/joeattardi/github-pr-dashboard">
-        GitHub Pull Request Dashboard v{pkg.version}
+        Forked from GitHub Pull Request Dashboard
       </a>
     </footer>
   );
